618d5f873912c89d508f4e61913da23aeca1ec2b
AlbinGGustafsson <abbguu@gmail.com>
2023-10-26 18:32:23 +0200

    Kommentarer klart

--- src/main/java/org/server/ServerGUI.java
+++ src/main/java/org/server/ServerGUI.java
@@ -31,7 +31,7 @@ public class ServerGUI extends JFrame {
 
     /**
      * Konstruktor för ServerGUI-klassen.
-     * Sätter upp gränsnsittet.
+     * Sätter upp gränssnittet.
      */
     public ServerGUI() {
 
